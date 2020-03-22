a301a34c0dc95f27c094ee00b320e35992535678
Douglas Garcia <douglas.garcia@fs.com.br>
2020-03-22 06:15:33 -0300

    Novo Report

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,16 +1,40 @@
-# Sometimes it's a README fix, or something like that - which isn't relevant for
-# including in a project's CHANGELOG for example
-declared_trivial = github.pr_title.include? "#trivial"
 
-# Make it more obvious that a PR is a work in progress and shouldn't be merged yet
-warn("PR is classed as Work in Progress") if github.pr_title.include? "[WIP]"
+import Danger
 
-# Warn when there is a big PR
-warn("Big PR") if git.lines_of_code > 300
-warn("Large PR") if git.lines_of_code > 500
-warn("Huge PR") if git.lines_of_code > 700
-warn("Freakin Huge PR") if git.lines_of_code > 1000
+let danger = Danger()
 
-# Don't let testing shortcuts get into master by accident
-fail("fdescribe left in tests") if `grep -r fdescribe specs/ `.length > 1
-fail("fit left in tests") if `grep -r fit specs/ `.length > 1
+// Pull request size
+let bigPRThreshold = 0
+let additions = danger.github.pullRequest.additions!
+let deletions = danger.github.pullRequest.deletions!
+let changedFiles = danger.github.pullRequest.changedFiles!
+if (additions + deletions > bigPRThreshold) {
+    warn("PR size seems relatively large. ✂️ If this PR contains multiple changes, please split each into separate PR will helps faster, easier review.")
+}
+
+// Pull request body validation
+if danger.github.pullRequest.body == nil || danger.github.pullRequest.body!.isEmpty {
+    warn("PR has no description. 📝 You should provide a description of the changes that have made.")
+}
+
+// Pull request title validation
+let prTitle = danger.github.pullRequest.title
+if prTitle.contains("WIP") {
+    warn("PR is classed as _Work in Progress_.")
+}
+if prTitle.count < 5 {
+    warn("PR title is too short. 🙏 Please use this format `[SDK-000] Your feature title` and replace `000` with Jira task number.")
+}
+if !prTitle.contains("[SDK-") {
+    warn("PR title does not containe the related Jira task. 🙏 Please use this format `[SDK-000] Your feature title` and replace `000` with Jira task number.")
+}
+
+// Files changed and created should includes unit tests
+let modified = danger.git.modifiedFiles
+let editedFiles = modified + danger.git.createdFiles
+let testFiles = editedFiles.filter { ($0.contains("Tests") || $0.contains("Test")) && ($0.fileType == .swift  || $0.fileType == .m) }
+if testFiles.isEmpty {
+    warn("PR does not contain any files related to unit tests ✅ (ignore if your changes do not require tests)")
+}
+
+message("🎉 The PR added \(additions) and removed \(deletions) lines. 🗂 \(changedFiles) files changed.")
